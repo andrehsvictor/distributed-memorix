f4760ba968bc4e36951c34f15614289e67c7e4bb
André Victor <andrehsvictor@gmail.com>
2025-08-30 10:32:55 -0300

    feat: add initial project structure with Maven configuration and application files

---
+++ deck-service/src/main/java/io/github/andrehsvictor/deck/service/DeckServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.andrehsvictor.deck.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckServiceApplication.class, args);
+	}
+
+}
